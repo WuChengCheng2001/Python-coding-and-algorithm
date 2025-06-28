63632f8f818c2bdaf89076c1322c4c1edf440fa3
WuChengcheng <61872108+WuChengCheng2001@users.noreply.github.com>
2025-06-28 16:52:35 +0800

    Add files via upload

---
+++ 代码随想录/数组/3-长度最小的子数组.py
@@ -0,0 +1,31 @@
+'''给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，并返回其长度。如果不存在符合条件的子数组，返回 0。
+
+示例：
+
+输入：s = 7, nums = [2,3,1,2,4,3]
+输出：2
+解释：子数组 [4,3] 是该条件下的长度最小的子数组。'''
+
+# 关键词：
+# 快慢指针法
+# 除了适用于“原地修改数组”外，还可以解决“满足特定条件的连续子数组”问题
+
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        l = len(nums)
+        left = 0
+        right = 0
+        min_len = float('inf')
+        cur_sum = 0 #当前的累加值
+        
+        while right < l:
+            cur_sum += nums[right]
+            
+            while cur_sum >= s: # 当前累加值大于目标值
+                min_len = min(min_len, right - left + 1)
+                cur_sum -= nums[left]
+                left += 1
+            
+            right += 1
+        
+        return min_len if min_len != float('inf') else 0
